b7dcbaa0655cbb09068ba4460b8622ffbab9f45e
luis.liberati <luis.liberati@tivit.com>
2021-11-12 11:12:32 -0300

    Post category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -35,5 +35,15 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada"));
 		return new CategoryDTO(entity);
 	}
+
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		
+		repository.save(entity);
+		return new CategoryDTO(entity);
+	}
 	
 }
